2eefe06d8878950ec14b2c0b9452f9d0f8b90f2e
steiner <steiner3044@163.com>
2022-07-20 15:08:20 +0800

    change point type

--- src/point.jl
+++ src/point.jl
@@ -1,3 +1,6 @@
 # Point = NamedTuple{(:x, :y), Tuple{<:Number, <:Number}}
 # Point = NamedTuple{(:x, :y), T} where T <: Tuple{Number, Number}
-Point = NamedTuple{(:x, :y), Tuple{T1, T2}} where {T1 <: Number, T2 <: Number}
+struct Point
+  x::T1 where T1 <: Number
+  y::T2 where T2 <: Number
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
 using Geometric, Test, LinkedList
 
 @testset "test intersection" begin
-  P3 = (x = -0.5, y = 3)
-  P4 = (x = 1.5, y = 0.5)
+  P3 = Point(-0.5, 3)
+  P4 = Point(1.5, 0.5)
 
-  P1 = (x = -4, y = -2)
-  P2 = (x = 4, y = 2)
+  P1 = Point(-4, -2)
+  P2 = Point(4, 2)
 
   @test intersection(P1, P2, P3, P4) == true
 end
@@ -22,7 +22,8 @@ end
     (x = 0, y = -2),
     (x = 2, y = 0),
     (x = 4, y = 1)
-  ]
+  ] |> points -> map(point -> Point(point.x, point.y), points)
+    
 
   points = List(Point)
   for point in pointsarray
